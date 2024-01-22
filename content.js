6a12de96d769946ec746d94fa6c1cd9efe71b137
Wadeck Follonier <wadeck.follonier@gmail.com>
2024-01-22 21:13:49 +0100

    Correct the bug with count===0, and adjust position with new GH design
    Resolves #1

--- content.js
+++ content.js
@@ -40,17 +40,17 @@ async function getTickets(org, repo, state, tabNo) {
         return [];
     }
 
-    if (count < 0) {
+    if (count <= 0) {
         return [];
     }
 
     const allEntries = Array.from(fakeEl.querySelectorAll('#advisories .lh-condensed>a'));
     if (allEntries.length === 0) {
         console.error(`[content.js#getTickets(${org}, ${repo}, ${state}, ${tabNo})] No advisories found despite a valid count=${count}`);
-        return;
+        return [];
     } else if (allEntries.length !== count) {
         console.error(`[content.js#getTickets(${org}, ${repo}, ${state}, ${tabNo})] Advisories found (${allEntries.length}) not equal to the count=${count}`);
-        return;
+        return [];
     }
 
     const allAdvisoryNames = allEntries.map(el => el.text.trim());
@@ -81,7 +81,8 @@ async function main() {
 
         if (triageTickets.length > 0 || draftTickets.length > 0) {
             // the nav is the horizontal bar with Code | Issues | Pull requests | [...]
-            const nav = document.querySelector('main nav');
+            // const nav = document.querySelector('main nav');
+            const nav = document.querySelector('header nav.UnderlineNav');
             const bannerBuilder = document.createElement('div');
 
             const warnIcon = '<svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor" style="display:inline-block;vertical-align:text-bottom" class="mr-1"><path d="M6.457 1.047c.659-1.234 2.427-1.234 3.086 0l6.082 11.378A1.75 1.75 0 0 1 14.082 15H1.918a1.75 1.75 0 0 1-1.543-2.575Zm1.763.707a.25.25 0 0 0-.44 0L1.698 13.132a.25.25 0 0 0 .22.368h12.164a.25.25 0 0 0 .22-.368Zm.53 3.996v2.5a.75.75 0 0 1-1.5 0v-2.5a.75.75 0 0 1 1.5 0ZM9 11a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"></path></svg>';
@@ -89,7 +90,7 @@ async function main() {
             const xIcon = '<svg class="octicon octicon-x" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">  <path fill-rule="evenodd" clip-rule="evenodd" d="M3.72 3.72C3.86062 3.57955 4.05125 3.50066 4.25 3.50066C4.44875 3.50066 4.63937 3.57955 4.78 3.72L8 6.94L11.22 3.72C11.2887 3.64631 11.3715 3.58721 11.4635 3.54622C11.5555 3.50523 11.6548 3.48319 11.7555 3.48141C11.8562 3.47963 11.9562 3.49816 12.0496 3.53588C12.143 3.5736 12.2278 3.62974 12.299 3.70096C12.3703 3.77218 12.4264 3.85702 12.4641 3.9504C12.5018 4.04379 12.5204 4.14382 12.5186 4.24452C12.5168 4.34523 12.4948 4.44454 12.4538 4.53654C12.4128 4.62854 12.3537 4.71134 12.28 4.78L9.06 8L12.28 11.22C12.3537 11.2887 12.4128 11.3715 12.4538 11.4635C12.4948 11.5555 12.5168 11.6548 12.5186 11.7555C12.5204 11.8562 12.5018 11.9562 12.4641 12.0496C12.4264 12.143 12.3703 12.2278 12.299 12.299C12.2278 12.3703 12.143 12.4264 12.0496 12.4641C11.9562 12.5018 11.8562 12.5204 11.7555 12.5186C11.6548 12.5168 11.5555 12.4948 11.4635 12.4538C11.3715 12.4128 11.2887 12.3537 11.22 12.28L8 9.06L4.78 12.28C4.63782 12.4125 4.44977 12.4846 4.25547 12.4812C4.06117 12.4777 3.87579 12.399 3.73837 12.2616C3.60096 12.1242 3.52225 11.9388 3.51882 11.7445C3.51539 11.5502 3.58752 11.3622 3.72 11.22L6.94 8L3.72 4.78C3.57955 4.63938 3.50066 4.44875 3.50066 4.25C3.50066 4.05125 3.57955 3.86063 3.72 3.72Z"></path></svg>';
 
             bannerBuilder.innerHTML = `
-<div class="jenkins-security-banner color-bg-subtle px-3 px-md-4 px-lg-5">
+<div class="jenkins-security-banner px-3 px-md-4 px-lg-5">
     <div data-view-component="true" class="flash flash-warn">
         <button id="ejs-close" class="flash-close js-flash-close" type="button" aria-label="Close" onClick="closePopup()">
             <!-- <%= octicon "x" %> -->
